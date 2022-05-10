e55da3704c36f3bfc0f188bbde7bdee4eedd8825
JaimeBravoAlgaba <jaime.bravo.algaba@gmail.com>
2022-05-10 16:08:31 +0200

    Basic data receiving procedure.

--- include/MotorService.h
+++ include/MotorService.h
@@ -1,9 +1,11 @@
 #ifndef __MOTOR_SERVICE__
     #define __MOTOR_SERVICE__
 
+    #define SPEED  150
+
     // PINOUT:
-    #define MOTOR_A D4  // Motor, input A.
-    #define MOTOR_B D3  // Motor, input B.
+    #define MOTOR_A D3  // Motor, input A.
+    #define MOTOR_B D4  // Motor, input B.
     #define MOTOR_S D5  // Motor, speed (PWM).
 
     // FUNCTIONS:
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,77 @@
 #include <Arduino.h>
+#include <MotorService.h>
+#include <ESP8266WiFi.h>
+#include <Secrets.h>
+#include <WiFiUdp.h>
+
+// Variables WiFi:
+WiFiUDP Udp;
+unsigned int localUdpPort = 4210;  // local port to listen on
+char incomingPacket[255];  // buffer for incoming packets
+char  replyPacket[] = "OK";  // a reply string to send back
+
+/**
+ * @brief Parpadea el LED de la placa NodeMCU.
+ * 
+ * @param period En milisegundos.
+ * @param iters Número de parpadeos.
+ */
+void blink(int period, int iters){
+  for(int i=0; i<iters; i++){
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(period/2);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(period/2);
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  // Motors setup:
+  setupMotor();
+
+  // WiFi setup:
+  Serial.println("Connecting to " + String(SSID) + "...");
+  WiFi.config(IPAddress(LOCAL_IP), IPAddress(GATEWAY), IPAddress(SUBNET));
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+    blink(10, 1);
+  }
+  Serial.println(" Connected!");
+  blink(150, 2);
+
+  Udp.begin(localUdpPort);
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  String payload;
+  
+  int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    int len = Udp.read(incomingPacket, 255);
+    if (len > 0)
+    {
+      incomingPacket[len] = 0;
+    }
+    payload = String(incomingPacket);
+    //Serial.println("Payload: " + payload);
+
+    // send back a reply, to the IP address and port we got the packet from
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(replyPacket);
+    Udp.endPacket();
+  }
+
+  if      (payload.compareTo("UP"))     { moveMotor( SPEED); }
+  else if (payload.compareTo("DOWN"))   { moveMotor(-SPEED); }
+  else if (payload.compareTo("STOP"))   { moveMotor(0); }
+  else if (payload.compareTo("GRAB"))   {}
+  else if (payload.compareTo("RELEASE")){}
+  else { moveMotor(0); }
 }
